d004b307007f3059d5dfaccbd62e995fba8a3bb6
Doug Jones <DSJR1284@GMAIL.COM>
2021-11-22 21:27:04 -0500

    commit tree 3c1fc07e3b9a4e684fe3d1b0570029ff7b789d23

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,47 @@
-let state;
+//Action -> Reducer -> New State.
+//Closure Function
+/* This feature is called a closure since a
+function encloses or draws a protective bubble around the variables in its scope
+and carries those with it when invoked later. */
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { 
+    dispatch, 
+    getState
+  };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
     default:
       return state;
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(reducer);
+store.dispatch
+({type:'@@INIT'});
+
+// dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
